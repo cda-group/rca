31fcae6199a79cfc215508a1084139d79c43a3e6
Max Meldrum <max@meldrum.se>
2018-11-06 13:38:13 +0100

    Add target triple and json printing

--- src/rca.c
+++ src/rca.c
@@ -1,4 +1,12 @@
+// Runtime Compiler Assistent
+// Gathers machine information to be sent to the runtime
+// in order for remote cross compilation to happen.
+// ---------------------------------------------
+// JSON logic extracted from the cpu_features example (APACHE V2 LICENSE)
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "cpu_features_macros.h"
 #include "cpuinfo_aarch64.h"
@@ -7,12 +15,162 @@
 #include "cpuinfo_ppc.h"
 #include "cpuinfo_x86.h"
 
-int main() {
+static void PrintEscapedAscii(const char* str) {
+  putchar('"');
+  for (; str && *str; ++str) {
+    switch (*str) {
+      case '\"':
+      case '\\':
+      case '/':
+      case '\b':
+      case '\f':
+      case '\n':
+      case '\r':
+      case '\t':
+        putchar('\\');
+    }
+    putchar(*str);
+  }
+  putchar('"');
+}
+
+static void PrintVoid(void) {}
+static void PrintComma(void) { putchar(','); }
+static void PrintLineFeed(void) { putchar('\n'); }
+static void PrintOpenBrace(void) { putchar('{'); }
+static void PrintCloseBrace(void) { putchar('}'); }
+static void PrintOpenBracket(void) { putchar('['); }
+static void PrintCloseBracket(void) { putchar(']'); }
+static void PrintString(const char* field) { printf("%s", field); }
+static void PrintAlignedHeader(const char* field) { printf("%-15s : ", field); }
+static void PrintIntValue(int value) { printf("%d", value); }
+static void PrintDecHexValue(int value) {
+  printf("%3d (0x%02X)", value, value);
+}
+static void PrintJsonHeader(const char* field) {
+  PrintEscapedAscii(field);
+  putchar(':');
+}
+
+typedef struct {
+  void (*Start)(void);
+  void (*ArrayStart)(void);
+  void (*ArraySeparator)(void);
+  void (*ArrayEnd)(void);
+  void (*PrintString)(const char* value);
+  void (*PrintValue)(int value);
+  void (*EndField)(void);
+  void (*StartField)(const char* field);
+  void (*End)(void);
+} Printer;
+
+static Printer getJsonPrinter(void) {
+  return (Printer){
+      .Start = &PrintOpenBrace,
+      .ArrayStart = &PrintOpenBracket,
+      .ArraySeparator = &PrintComma,
+      .ArrayEnd = &PrintCloseBracket,
+      .PrintString = &PrintEscapedAscii,
+      .PrintValue = &PrintIntValue,
+      .EndField = &PrintComma,
+      .StartField = &PrintJsonHeader,
+      .End = &PrintCloseBrace,
+  };
+}
+
+// Prints a named numeric value in both decimal and hexadecimal.
+static void PrintN(const Printer p, const char* field, int value) {
+  p.StartField(field);
+  p.PrintValue(value);
+  p.EndField();
+}
+
+// Prints a named string.
+static void PrintS(const Printer p, const char* field, const char* value) {
+  p.StartField(field);
+  p.PrintString(value);
+  p.EndField();
+}
+
+static int cmp(const void* p1, const void* p2) {
+  return strcmp(*(const char* const*)p1, *(const char* const*)p2);
+}
+
+#define DEFINE_PRINT_FLAGS(HasFeature, FeatureName, FeatureType, LastEnum) \
+  static void PrintFlags(const Printer p, const FeatureType* features) {   \
+    size_t i;                                                              \
+    const char* ptrs[LastEnum] = {0};                                      \
+    size_t count = 0;                                                      \
+    for (i = 0; i < LastEnum; ++i) {                                       \
+      if (HasFeature(features, i)) {                                       \
+        ptrs[count] = FeatureName(i);                                      \
+        ++count;                                                           \
+      }                                                                    \
+    }                                                                      \
+    qsort(ptrs, count, sizeof(char*), cmp);                                \
+    p.StartField("flags");                                                 \
+    p.ArrayStart();                                                        \
+    for (i = 0; i < count; ++i) {                                          \
+      if (i > 0) p.ArraySeparator();                                       \
+      p.PrintString(ptrs[i]);                                              \
+    }                                                                      \
+    p.ArrayEnd();                                                          \
+  }
+
+#if defined(CPU_FEATURES_ARCH_X86)
+DEFINE_PRINT_FLAGS(GetX86FeaturesEnumValue, GetX86FeaturesEnumName, X86Features,
+                   X86_LAST_)
+#elif defined(CPU_FEATURES_ARCH_ARM)
+DEFINE_PRINT_FLAGS(GetArmFeaturesEnumValue, GetArmFeaturesEnumName, ArmFeatures,
+                   ARM_LAST_)
+#elif defined(CPU_FEATURES_ARCH_AARCH64)
+DEFINE_PRINT_FLAGS(GetAarch64FeaturesEnumValue, GetAarch64FeaturesEnumName,
+                   Aarch64Features, AARCH64_LAST_)
+#elif defined(CPU_FEATURES_ARCH_MIPS)
+DEFINE_PRINT_FLAGS(GetMipsFeaturesEnumValue, GetMipsFeaturesEnumName,
+                   MipsFeatures, MIPS_LAST_)
+#elif defined(CPU_FEATURES_ARCH_PPC)
+DEFINE_PRINT_FLAGS(GetPPCFeaturesEnumValue, GetPPCFeaturesEnumName, PPCFeatures,
+                   PPC_LAST_)
+#endif
+
+
+/**
+ * Fetches target triple that has been set externally as 
+ * environment variable through gcc -dumpmachine
+ */
+char* get_target_triple() {
+    char *triple = getenv("TARGET_TRIPLE");
+    if (triple)
+        return triple;
+    else
+        return "unknown";
+}
+
+void generate_compiler_info(const Printer printer) {
+    char *triple = get_target_triple();
 #if defined(CPU_FEATURES_ARCH_X86)
     const X86Info info = GetX86Info();
-    printf("X86\n");
+    char brand_string[49];
+    FillX86BrandString(brand_string);
+    PrintS(printer, "triple", triple);
+	PrintS(printer, "arch", "x86");
+    PrintS(printer, "uarch", GetX86MicroarchitectureName(GetX86Microarchitecture(&info)));
+	PrintS(printer, "brand", brand_string);
+    PrintFlags(printer, &info.features);
 #elif defined(CPU_FEATURES_ARCH_ARM)
-    printf("ARCH_ARM\n");
+    // Just X86 for now..
 #endif
-    return 0;
+
 }
+
+int main() {
+	Printer printer = getJsonPrinter();
+	printer.Start();
+	generate_compiler_info(printer);
+	printer.End();
+    PrintLineFeed();
+    return EXIT_SUCCESS;
+}
+
+
